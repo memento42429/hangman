434a8d96c2559311b695a5f74fe5f29e9eec2bd1
Kent Mori <46188183+memento42429@users.noreply.github.com>
2022-08-11 11:16:11 +0900

    ver1 it doesnt work well.

--- movingave.py
+++ movingave.py
@@ -2,4 +2,16 @@ import pandas_datareader.data as pdr
 df = pdr.DataReader("PFE.US","stooq").sort_index()
 data = df.loc['2021-3-22':'2021-08-11', 'Close' ].tolist()
 
-data
+def movingave(data,periods):
+    n = len(data)
+    ave = [0]*(n - periods+1)
+    for i in range(n - periods+1):
+        t = data[i:i+periods]
+        sumt = 0
+        for j in t:
+            sumt += j
+        ave[i] = round(sumt/periods,2)
+    return ave
+    
+print(movingave(data,5))
+
